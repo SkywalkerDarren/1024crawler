c8c81d68df2f24666457d634980a8618219acaa5
SkywlkerDarren <yh322yh@163.com>
2017-02-09 20:37:09 +0800

    download picture

--- 1024.py
+++ 1024.py
@@ -1,13 +1,20 @@
-#-*- coding:utf-8  -*-
+# -*- coding:utf-8  -*-
 import re
 from bs4 import BeautifulSoup
 import urllib.request
 import urllib.error
 import time
+import os
 
-def Urlreq(url):
+
+PATH = 'D:\\Media\\'
+ROOTURL = 'http://www.t66y.com/'
+
+
+def urlRequest(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/55.0.2883.87 Safari/537.36',
         'Accept-Encoding': 'gzip, deflate, sdch',
         'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.6',
         'Connection': 'keep-alive',
@@ -16,46 +23,66 @@ def Urlreq(url):
     while test:
         try:
             request = urllib.request.Request(url, headers=headers)
-            response = urllib.request.urlopen(request, timeout=3)
+            response = urllib.request.urlopen(request, timeout=5)
             test = False
             return response
-        except:
-            print("timeout")
+        except Exception as e:
+            print(Exception, ':', e)
 
 
-
-def Urlspider(page):
-    urldir = 'http://sbl8.xyz/thread0806.php?fid=8&search=&type=1&page='
-    url = urldir + str(page)
-    response = Urlreq(url)
+def urlSpider(page):
+    urlDir = ROOTURL + 'thread0806.php?fid=8&search=&type=1&page='
+    url = urlDir + str(page)
+    response = urlRequest(url)
     soup = BeautifulSoup(response.read(), 'html.parser')
-    urllist = soup.find_all(href=re.compile('htm_data'), id=re.compile(''))
-    length = len(urllist)
+    urlList = soup.find_all(href=re.compile('htm_data'), id=re.compile(''))
+    length = len(urlList)
     for item in range(length):
-        urlin = 'http://sbl8.xyz/' + urllist[item].attrs['href']
-        print(urlin)
-        Picspider(urlin)
+        picUrl = ROOTURL + urlList[item].attrs['href']
+
+        print(urlList[item].text)
+        path = PATH + urlList[item].text + '\\'
+        try:
+            os.makedirs(path)
+        except:
+            pass
+        print(picUrl)
+        picSpider(picUrl, path)
+
         if item % 20 == 0:
             time.sleep(3)
-        print(urllist[item].text)
 
 
-def Download(url, num):
-    f = Urlreq(url)
+def download(url, num, path):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/55.0.2883.87 Safari/537.36',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.6',
+        'Connection': 'keep-alive',
+    }
+    request = urllib.request.Request(url, headers=headers)
+    response = urllib.request.urlopen(request, timeout=60)
+    img = response
+    response.close()
+    time.sleep(3)
+    f = open(path + str(num) + '.jpg', 'wb')
+    f.write(img.read())
+    f.close()
 
 
+def picSpider(url, path):
+    response = urlRequest(url)
+    content = response.read().decode('gbk')
+    pattern = re.compile('input src=\'(.*?)\' type=\'image\'', re.S)
+    items = re.findall(pattern, content)
+    cnt = 0
+    for item in items:
+        print(item)
+        cnt = cnt + 1
+        download(item, cnt, path)
 
-def Picspider(url):
-    for test in range(3):
-        response = Urlreq(url)
-        if response != 0:
-            content = response.read().decode('gbk')
-            pattern = re.compile('input src=\'(.*?)\' type=\'image\'', re.S)
-            items = re.findall(pattern, content)
-            for item in items:
-                print(item)
-            break
 
 if __name__ == "__main__":
     for i in range(3):
-        Urlspider(i + 1)
+        urlSpider(i + 1)
